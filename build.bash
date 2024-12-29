a3455519b9b0a40a354c2353b245649438b9a272
illy <fluoriteByte@proton.me>
2024-12-29 15:12:54 +0100

    change some stuffs

--- build.bash
+++ build.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-host-shell flatpak-builder build org.virtualbox.VirtualBox-KVM.yml \
+flatpak run org.flatpak.Builder build org.virtualbox.VirtualBox-KVM.yml \
 	--force-clean \
 	--ccache \
 	--disable-rofiles-fuse \
