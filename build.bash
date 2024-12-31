eff269580e20199094ab25127d4649a7017ff5f8
illy <fluoriteByte@proton.me>
2024-12-31 18:44:21 +0100

    add metainfo

--- build.bash
+++ build.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+flatpak run --command=flatpak-builder-lint org.flatpak.Builder appstream files/org.virtualbox.VirtualBox_KVM.metainfo.xml || exit
+
 flatpak run org.flatpak.Builder build org.virtualbox.VirtualBox_KVM.yml \
 	--force-clean \
 	--ccache \
